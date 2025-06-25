3044ae43b12a389ea6e2865a6acb74f44ddae8c8
benny <vvsogi@gmail.com>
2025-06-25 16:19:38 +0900

    Remove "Hello world!" from index.html and implement CanvasManager class for canvas handling in canvas-manager.ts.

--- src/managers/canvas-manager.ts
+++ src/managers/canvas-manager.ts
@@ -1,1 +1,36 @@
-export class CanvasManager {}
+export class CanvasManager {
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+  private stageWidth!: number;
+  private stageHeight!: number;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) throw new Error("Canvas context not available");
+
+    this.ctx = ctx;
+
+    this.resize();
+    window.addEventListener("resize", this.resize);
+
+    requestAnimationFrame(this.draw);
+  }
+
+  private resize = () => {
+    this.stageWidth = this.canvas.clientWidth;
+    this.stageHeight = this.canvas.clientHeight;
+
+    this.canvas.width = this.stageWidth * 2;
+    this.canvas.height = this.stageHeight * 2;
+    this.ctx.scale(2, 2);
+  };
+
+  private draw = (t: number) => {
+    requestAnimationFrame(this.draw);
+
+    this.ctx.arc(this.stageWidth / 2, this.stageHeight / 2, 10, 0, Math.PI * 2);
+    this.ctx.fillStyle = "black";
+    this.ctx.fill();
+  };
+}
