d87f95481c1a39fc93bf7968f5cc014846f4c87e
benny <vvsogi@gmail.com>
2025-06-27 15:22:40 +0900

    Add DragTool implementation and enhance BaseTool with resize and draw methods

---
+++ src/tools/drag-tool.ts
@@ -0,0 +1,68 @@
+import { MousePoint } from "../types/common";
+import { MouseUtils } from "../utils";
+import { BaseTool } from "./base-tool";
+
+interface DragRange {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+export class DragTool extends BaseTool {
+  public readonly name = "drag";
+  private initPoint: MousePoint | null = null;
+  private movePoint: MousePoint | null = null;
+
+  constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    super(canvas, ctx);
+  }
+
+  onMouseDown = (e: MouseEvent) => {
+    this.isDrawing = true;
+    const position = MouseUtils.getMousePos(e, this.canvas);
+    this.initPoint = position;
+    this.movePoint = position;
+  };
+
+  onMouseMove = (e: MouseEvent) => {
+    this.movePoint = MouseUtils.getMousePos(e, this.canvas);
+  };
+
+  onMouseUp = (e: MouseEvent) => {
+    this.isDrawing = false;
+
+    this.initPoint = null;
+    this.movePoint = null;
+  };
+
+  draw = (): DragRange | null => {
+    if (!this.initPoint || !this.movePoint) return null;
+
+    const { x, y } = this.movePoint;
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, y);
+    this.ctx.lineTo(this.initPoint.x, y);
+    this.ctx.lineTo(this.initPoint.x, this.initPoint.y);
+    this.ctx.lineTo(x, this.initPoint.y);
+    this.ctx.lineTo(x, y);
+    this.ctx.fill();
+    this.ctx.fillStyle = "rgba(105, 105, 230, 0.1)";
+    this.ctx.strokeStyle = "rgba(105, 105, 230)";
+    this.ctx.stroke();
+    this.ctx.closePath();
+
+    const left = Math.min(x, this.initPoint.x);
+    const top = Math.min(y, this.initPoint.y);
+    const right = Math.max(x, this.initPoint.x);
+    const bottom = Math.max(y, this.initPoint.y);
+
+    return {
+      x1: left,
+      y1: top,
+      x2: right,
+      y2: bottom,
+    };
+  };
+}
--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,2 +1,3 @@
 export * from "./base-tool";
+export * from "./drag-tool";
 export * from "./line-tool";
