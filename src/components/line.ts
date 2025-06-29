018322864b9d0800e4c9037913257f8e274f70cd
benny <vvsogi@gmail.com>
2025-06-29 16:28:28 +0900

    Refactor Line component to use LinePosition interface and add drag effect functionality

--- src/components/line.ts
+++ src/components/line.ts
@@ -1,30 +1,55 @@
-import { BaseComponent } from "./base-component";
+import { BaseComponent, BasePosition } from "./base-component";
+
+interface LinePosition extends BasePosition {
+  cx: number;
+  cy: number;
+}
 
 interface Props {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
-  position: {
-    x1: number;
-    y1: number;
-    cx: number;
-    cy: number;
-    x2: number;
-    y2: number;
-  };
+  position: LinePosition;
 }
 
-export class Line extends BaseComponent {
-  name: string = "line";
-  position: Props["position"];
+export class Line extends BaseComponent<LinePosition> {
+  readonly name: string = "line";
 
   constructor({ canvas, ctx, position }: Props) {
-    super(canvas, ctx);
-    this.position = position;
+    super(canvas, ctx, position);
   }
 
   onMouseDown = (e: MouseEvent) => {};
   onMouseMove = (e: MouseEvent) => {};
   onMouseUp = (e: MouseEvent) => {};
+
+  getPosition = (): BasePosition => {
+    return this.position;
+  };
+
+  setDragState = (state: boolean) => {
+    this.isDrag = state;
+    return this.isDrag;
+  };
+
+  private dragEffect = () => {
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.arc(this.position.x1, this.position.y1, 5, 0, Math.PI * 2);
+    this.ctx.arc(this.position.x2, this.position.y2, 5, 0, Math.PI * 2);
+    this.ctx.fillStyle = "rgba(105, 105, 230, 0.5)";
+    this.ctx.fill();
+    this.ctx.closePath();
+    this.ctx.restore();
+
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.arc(this.position.x1, this.position.y1, 2.5, 0, Math.PI * 2);
+    this.ctx.arc(this.position.x2, this.position.y2, 2.5, 0, Math.PI * 2);
+    this.ctx.fillStyle = "#ffffff";
+    this.ctx.fill();
+    this.ctx.closePath();
+    this.ctx.restore();
+  };
 
   draw = () => {
     this.ctx.beginPath();
@@ -33,5 +58,9 @@ export class Line extends BaseComponent {
     this.ctx.strokeStyle = "black";
     this.ctx.stroke();
     this.ctx.closePath();
+
+    if (this.isDrag) {
+      this.dragEffect();
+    }
   };
 }
