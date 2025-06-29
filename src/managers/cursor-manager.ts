a20802115b27765d37284cad31a4220351ce38df
benny <vvsogi@gmail.com>
2025-06-29 18:18:35 +0900

    Add CursorManager class for handling canvas cursor states

---
+++ src/managers/cursor-manager.ts
@@ -0,0 +1,55 @@
+export class CursorManager {
+  public currentCursor: "default" | "move" | "pointer" = "default";
+
+  protected canvas: HTMLCanvasElement;
+  protected ctx: CanvasRenderingContext2D;
+
+  constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.activate();
+  }
+
+  public activate = () => {
+    this.addEventListeners();
+  };
+
+  public deactivate = () => {
+    this.removeEventListeners();
+  };
+
+  public setDefault = () => {
+    this.currentCursor = "default";
+    this.canvas.style.cursor = "default";
+  };
+
+  public setMove = () => {
+    this.currentCursor = "move";
+    this.canvas.style.cursor = "move";
+  };
+
+  public setPointer = () => {
+    this.currentCursor = "pointer";
+    this.canvas.style.cursor = "pointer";
+  };
+
+  private onMouseMove = (e: MouseEvent) => {
+    this.setDefault();
+  };
+
+  private onMouseDown = (e: MouseEvent) => {};
+
+  private onMouseUp = (e: MouseEvent) => {};
+
+  private addEventListeners = () => {
+    this.canvas.addEventListener("mousedown", this.onMouseDown);
+    this.canvas.addEventListener("mousemove", this.onMouseMove);
+    this.canvas.addEventListener("mouseup", this.onMouseUp);
+  };
+
+  private removeEventListeners = () => {
+    this.canvas.removeEventListener("mousedown", this.onMouseDown);
+    this.canvas.removeEventListener("mousemove", this.onMouseMove);
+    this.canvas.removeEventListener("mouseup", this.onMouseUp);
+  };
+}
