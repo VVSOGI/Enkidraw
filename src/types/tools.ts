59fbc0fe900ab06629722c666e992756ec1f6f7f
benny <vvsogi@gmail.com>
2025-07-22 14:40:41 +0900

    Added “zoom” to the tool name and implemented zoom conversion logic for mouse coordinate calculation in the MouseUtils class.

--- src/types/tools.ts
+++ src/types/tools.ts
@@ -1,5 +1,5 @@
 import { BaseTool } from "../tools";
 
-export type ToolNames = "drag" | "line";
+export type ToolNames = "drag" | "line" | "zoom";
 
 export type ToolConstructor = new (props: any) => BaseTool;
