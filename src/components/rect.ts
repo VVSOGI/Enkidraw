08629c45c48e8ca18d7fe5187e688ea485ad0bb9
benny <vvsogi@gmail.com>
2025-08-13 17:52:13 +0900

    Rect 클래스에 호버 상태 및 마우스 클릭 감지 로직을 개선하고, 드래그 및 이동 기능을 추가하여 사각형의 상호작용을 향상시킴. hoverCornor 속성을 추가하고, 마우스 위치에 따른 제어점 감지 로직을 구현하여 사용자 경험을 개선함.

--- src/components/rect.ts
+++ src/components/rect.ts
@@ -10,10 +10,18 @@ export class Rect extends BaseComponent {
   private borderRadius = 10;
   private totalPadding = 10;
   private dragCornorRectSize = 10;
+  private hoverCornor = {
+    point: -1,
+    coordinates: { x: 0, y: 0 },
+  };
 
   constructor({ canvas, ctx, position, getZoomTransform }: BaseComponentProps<BasePosition>) {
     super({ canvas, ctx, position, getZoomTransform });
   }
+
+  getPosition = () => {
+    return this.position;
+  };
 
   multiDragEffect = () => {};
 
@@ -24,7 +32,12 @@ export class Rect extends BaseComponent {
     const transform = this.getZoomTransform();
     const { x: mouseX, y: mouseY } = MouseUtils.getLogicalMousePos(e, this.canvas, transform);
 
-    if (mouseX >= x1 && mouseX <= x2 && mouseY >= y1 && mouseY <= y2) {
+    if (
+      mouseX >= x1 - this.totalPadding * 2 &&
+      mouseX <= x2 + this.totalPadding * 2 &&
+      mouseY >= y1 - this.totalPadding * 2 &&
+      mouseY <= y2 + this.totalPadding * 2
+    ) {
       return true;
     }
 
@@ -36,23 +49,69 @@ export class Rect extends BaseComponent {
     const transform = this.getZoomTransform();
     const { x: mouseX, y: mouseY } = MouseUtils.getLogicalMousePos(e, this.canvas, transform);
 
-    if (mouseX >= x1 && mouseX <= x2 && mouseY >= y1 && mouseY <= y2) {
+    if (
+      mouseX >= x1 - this.totalPadding &&
+      mouseX <= x2 + this.totalPadding &&
+      mouseY >= y1 - this.totalPadding &&
+      mouseY <= y2 + this.totalPadding
+    ) {
       return true;
     }
 
     return false;
   };
 
-  hoverComponent = (e: MouseEvent, move: MousePoint) => {};
+  hoverComponent = (e: MouseEvent, move: MousePoint) => {
+    if (!this.isActive) return;
 
-  moveComponent = (e: MouseEvent, move: MousePoint) => {};
+    const hoverCornor = this.getMouseHitControlPoint(move);
+
+    if (hoverCornor.point !== -1) {
+      this.hoverCornor = hoverCornor;
+    } else {
+      this.hoverCornor = {
+        point: -1,
+        coordinates: { x: 0, y: 0 },
+      };
+    }
+  };
+
+  moveComponent = (e: MouseEvent, move: MousePoint) => {
+    const { x: moveX, y: moveY } = move;
+    const nextPosition = Object.assign({}, this.position);
+    nextPosition.x1 = this.originPosition.x1 + moveX;
+    nextPosition.y1 = this.originPosition.y1 + moveY;
+    nextPosition.x2 = this.originPosition.x2 + moveX;
+    nextPosition.y2 = this.originPosition.y2 + moveY;
+
+    this.position = nextPosition;
+  };
 
   resizeComponent = (mouseDistance: MousePoint, multiSelectRange: DragRange, edgeDirection: EdgeDirection) => {};
 
-  initialPosition = () => {};
+  initialPosition = () => {
+    this.originPosition = {
+      x1: this.position.x1,
+      y1: this.position.y1,
+      x2: this.position.x2,
+      y2: this.position.y2,
+    };
+  };
 
-  getPosition = () => {
-    return this.position;
+  hoverCornorEffect = () => {
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.roundRect(
+      this.hoverCornor.coordinates.x - this.dragCornorRectSize,
+      this.hoverCornor.coordinates.y - this.dragCornorRectSize,
+      this.dragCornorRectSize * 2,
+      this.dragCornorRectSize * 2,
+      10
+    );
+    this.ctx.fillStyle = STYLE_SYSTEM.SECONDARY;
+    this.ctx.fill();
+    this.ctx.closePath();
+    this.ctx.restore();
   };
 
   dragEffect = () => {
@@ -122,8 +181,52 @@ export class Rect extends BaseComponent {
     this.ctx.closePath();
     this.ctx.restore();
 
+    if (this.hoverCornor.point >= 0) {
+      this.hoverCornorEffect();
+    }
+
     if (this.isActive) {
       this.dragEffect();
     }
+  };
+
+  private getMouseHitControlPoint = (mouse: MousePoint) => {
+    const { x: mouseX, y: mouseY } = mouse;
+
+    const currentPosition = this.getPosition();
+
+    const leftTop = {
+      x: currentPosition.x1 - this.dragCornorRectSize,
+      y: currentPosition.y1 - this.dragCornorRectSize,
+    };
+
+    const rightTop = {
+      x: currentPosition.x2 + this.dragCornorRectSize,
+      y: currentPosition.y1 - this.dragCornorRectSize,
+    };
+
+    const leftBottom = {
+      x: currentPosition.x1 - this.dragCornorRectSize,
+      y: currentPosition.y2 + this.dragCornorRectSize,
+    };
+
+    const rightBottom = {
+      x: currentPosition.x2 + this.dragCornorRectSize,
+      y: currentPosition.y2 + this.dragCornorRectSize,
+    };
+
+    const cornors = [leftTop, rightTop, leftBottom, rightBottom];
+    const cornor = cornors.findIndex(
+      ({ x, y }) =>
+        mouseX >= x - this.totalPadding / 2 &&
+        mouseX <= x + this.totalPadding / 2 &&
+        mouseY >= y - this.totalPadding / 2 &&
+        mouseY <= y + this.totalPadding / 2
+    );
+
+    return {
+      point: cornor,
+      coordinates: cornor >= 0 ? cornors[cornor] : { x: 0, y: 0 },
+    };
   };
 }
