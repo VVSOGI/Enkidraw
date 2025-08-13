039ed5dd3dd4013d030e8d09af1116ef689fc52f
benny <vvsogi@gmail.com>
2025-08-13 14:43:38 +0900

    RectTool 클래스에 드로잉 기능을 추가하고, 마우스 이벤트 처리 로직을 개선하여 사각형 그리기 기능을 강화함. 초기화 및 리셋 메서드를 구현하여 도구 상태 관리를 향상시킴.

--- src/tools/rect-tool.ts
+++ src/tools/rect-tool.ts
@@ -1,9 +1,15 @@
+import { MousePoint } from "../types";
 import { BaseTool, BaseToolProps } from "./base-tool";
 
 interface RectToolProps extends BaseToolProps {}
 
 export class RectTool extends BaseTool {
   public readonly name: string = "rect";
+
+  private initPoint: MousePoint | null = null;
+  private movePoint: MousePoint | null = null;
+  private lineWidth = 4;
+  private borderRadius = 10;
 
   constructor({ canvas, ctx, selectTool, deleteCurrentTool, getZoomTransform, activeManager }: RectToolProps) {
     super({ canvas, ctx, selectTool, deleteCurrentTool, getZoomTransform, activeManager });
@@ -21,26 +27,50 @@ export class RectTool extends BaseTool {
     }
   };
 
-  reset = () => {};
+  reset = () => {
+    this.initPoint = null;
+    this.movePoint = null;
+  };
 
   onMouseDown = (e: MouseEvent) => {
-    console.log("Rect Down");
     this.isDrawing = true;
     this.activeManager.selectCurrentActive("line");
+    const position = this.getLogicalMousePos(e);
+    this.initPoint = position;
+    this.movePoint = position;
   };
 
   onMouseMove = (e: MouseEvent) => {
     if (this.isDrawing) {
-      console.log("Rect Drawing");
+      const position = this.getLogicalMousePos(e);
+      this.movePoint = position;
     } else {
-      console.log("Rect move");
     }
   };
 
   onMouseUp = (e: MouseEvent) => {
-    console.log("Rect up");
-    this.isDrawing = false;
+    if (this.isDrawing) {
+      this.isDrawing = false;
+    }
   };
 
-  draw = () => {};
+  draw = () => {
+    if (!this.isDrawing || !this.initPoint || !this.movePoint) return;
+
+    const { x: startX, y: startY } = this.initPoint;
+    const { x: endX, y: endY } = this.movePoint;
+
+    const width = endX - startX;
+    const height = endY - startY;
+
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.lineWidth = this.lineWidth;
+    this.ctx.lineCap = "round";
+    this.ctx.strokeStyle = "black";
+    this.ctx.roundRect(startX, startY, width, height, this.borderRadius);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.restore();
+  };
 }
