dc2a16951236183a46cbc464d16b34cec53e9384
benny <vvsogi@gmail.com>
2025-08-26 16:07:01 +0900

    화살표 도구(ArrowTool) 클래스에 기본 선과 각도를 그리는 기능을 추가하고, 선의 두께를 설정할 수 있도록 개선함.

--- src/tools/arrow-tool.ts
+++ src/tools/arrow-tool.ts
@@ -11,6 +11,8 @@ export class ArrowTool extends BaseTool {
   private initPoint: MousePoint | null = null;
   private movePoint: MousePoint | null = null;
   private points: MousePoint[] = [];
+  private lineWidth = 5;
+  private type: "line" | "curve" | "angle" = "angle";
 
   protected leftMenuManager: LeftMenuManager;
 
@@ -55,21 +57,14 @@ export class ArrowTool extends BaseTool {
 
   onKeyDown = (e: KeyboardEvent) => {};
 
-  draw = () => {
+  drawDefaultLine = () => {
     if (!this.initPoint || !this.movePoint) return;
-
     this.ctx.save();
-    this.ctx.beginPath();
-    this.ctx.arc(this.initPoint.x, this.initPoint.y, 2, 0, Math.PI * 2);
-    this.ctx.fillStyle = this.leftMenuManager.strokeColor;
-    this.ctx.fill();
-    this.ctx.closePath();
-
     this.ctx.beginPath();
     this.ctx.moveTo(this.initPoint.x, this.initPoint.y);
     this.ctx.lineTo(this.movePoint.x, this.movePoint.y);
     this.ctx.strokeStyle = this.leftMenuManager.strokeColor;
-    this.ctx.lineWidth = 10;
+    this.ctx.lineWidth = this.lineWidth;
     this.ctx.lineCap = "round";
     this.ctx.stroke();
     this.ctx.closePath();
@@ -92,6 +87,44 @@ export class ArrowTool extends BaseTool {
     this.ctx.stroke();
     this.ctx.closePath();
     this.ctx.restore();
+  };
+
+  drawAngleLine = () => {
+    if (!this.initPoint || !this.movePoint) return;
+    const centerX = this.initPoint.x + (this.movePoint.x - this.initPoint.x) / 2;
+
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = this.leftMenuManager.strokeColor;
+    this.ctx.lineWidth = this.lineWidth;
+    this.ctx.lineCap = "round";
+    this.ctx.moveTo(this.initPoint.x, this.initPoint.y);
+    this.ctx.lineTo(centerX, this.initPoint.y);
+    this.ctx.lineTo(centerX, this.movePoint.y);
+    this.ctx.lineTo(this.movePoint.x, this.movePoint.y);
+
+    const headLength = 20;
+    const headAngle = Math.PI / 6;
+
+    this.ctx.moveTo(this.movePoint.x, this.movePoint.y);
+    this.ctx.lineTo(this.movePoint.x - headLength, this.movePoint.y - headLength * headAngle);
+    this.ctx.moveTo(this.movePoint.x, this.movePoint.y);
+    this.ctx.lineTo(this.movePoint.x - headLength, this.movePoint.y + headLength * headAngle);
+    this.ctx.stroke();
+    this.ctx.closePath();
+    this.ctx.restore();
+  };
+
+  draw = () => {
+    if (!this.initPoint || !this.movePoint) return;
+
+    if (this.type === "line") {
+      this.drawDefaultLine();
+    }
+
+    if (this.type === "angle") {
+      this.drawAngleLine();
+    }
   };
 
   reset = () => {
